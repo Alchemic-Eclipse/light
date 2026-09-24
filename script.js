fffe635ba091fada7fb37e02f2659973972ff12e
Alchemic-Eclipse <alchemic.eclipse@proton.me>
2026-09-25 01:36:11 +0530

    Design changes (hero-figure and typography)

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+history.scrollRestoration = "manual"; // Do not remember position before refresh (fixes glitch)
+
 const hero = document.querySelector("#hero");
 
 window.addEventListener("load", () => {
